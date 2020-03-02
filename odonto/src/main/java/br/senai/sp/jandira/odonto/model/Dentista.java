c193d5d2c295836474c925a9b250b99d7ca68868
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2020-03-02 16:44:27 -0300

    criando as especialidades

--- odonto/src/main/java/br/senai/sp/jandira/odonto/model/Dentista.java
+++ odonto/src/main/java/br/senai/sp/jandira/odonto/model/Dentista.java
@@ -1,9 +1,14 @@
 package br.senai.sp.jandira.odonto.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -23,8 +28,19 @@ public class Dentista {
 	private String email;
 	private String telefone;
 	
+	@ManyToMany
+	@JoinTable(name = "tbl_dentista_especialidade", joinColumns = @JoinColumn(name = "dentista_codigo", referencedColumnName = "codigo"),
+				inverseJoinColumns = @JoinColumn(name = "especialidade_codigo", referencedColumnName = "codigo"))
+	private List<Especialidade> especialidades;
+	
 	public Long getCodigo() {
 		return codigo;
+	}
+	public List<Especialidade> getEspecialidades() {
+		return especialidades;
+	}
+	public void setEspecialidades(List<Especialidade> especialidades) {
+		this.especialidades = especialidades;
 	}
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
@@ -58,8 +74,5 @@ public class Dentista {
 		return "Dentista [codigo=" + codigo + ", nome=" + nome + ", cro=" + cro + ", email=" + email + ", telefone="
 				+ telefone + "]";
 	}
-	
-	
-	
 	
 }
